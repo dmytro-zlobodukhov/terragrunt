4838ad8b919ffc4b666eb803de34d5f89ecd4957
Dmytro Zlobodukhov <96909881+dmytro-zlobodukhov@users.noreply.github.com>
2022-09-11 18:11:33 +0300

    fixed script

--- aws/terra/eu-west-1/dev/aws_eks/node_groups/apps/scripts/eks-node-provisioner.sh
+++ aws/terra/eu-west-1/dev/aws_eks/node_groups/apps/scripts/eks-node-provisioner.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ##### INSTALL AWS SSM AGENT #####
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
